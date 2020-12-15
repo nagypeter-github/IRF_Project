65cf40fd52bd6a90b97c75f9b692092bb9f1f1f4
Peti <Peti@DESKTOP-SHEQHLK>
2020-12-15 22:01:33 +0100

    commit internal

--- IRF_project/IRF_project/Form1.cs
+++ IRF_project/IRF_project/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LumenWorks.Framework.IO.Csv;
 
 namespace IRF_project
 {
     public partial class Form1 : Form
     {
+        private List<IRF_project> irf = new List<IRF_project>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
